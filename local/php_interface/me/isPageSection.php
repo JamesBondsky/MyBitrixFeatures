f44e0a92388c1c98f87b72a81422aac63795c33c
Leo <33494928+sl3w@users.noreply.github.com>
2020-05-14 16:09:48 +0400

    isPageCatalogOrDetail

---
+++ local/php_interface/me/isPageSection.php
@@ -0,0 +1,13 @@
+<? //require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+global $APPLICATION;
+$url = $APPLICATION->GetCurPage(false);
+$iblockID = 3;
+	 
+function isSectionInCatalog($iblockID, $url) {
+    $breads = explode('/', $url);
+    $code = array_pop(array_filter($breads));
+    $arFilter = array("IBLOCK_ID" => $iblockID, "CODE" => $code);
+    $rsSections = CIBlockSection::GetList(array(), $arFilter);
+    $isSection = $rsSections->Fetch() !== false;
+}
