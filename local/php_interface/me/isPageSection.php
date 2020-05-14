c4c1a0ece23683b3a85d71d6d9a43aac1f1d510d
Leo <33494928+sl3w@users.noreply.github.com>
2020-05-14 16:14:19 +0400

    Update isPageSection.php

--- local/php_interface/me/isPageSection.php
+++ local/php_interface/me/isPageSection.php
@@ -1,13 +1,16 @@
 <? //require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
-global $APPLICATION;
-$url = $APPLICATION->GetCurPage(false);
-$iblockID = 3;
-	 
-function isSectionInCatalog($iblockID, $url) {
-    $breads = explode('/', $url);
-    $code = array_pop(array_filter($breads));
-    $arFilter = array("IBLOCK_ID" => $iblockID, "CODE" => $code);
-    $rsSections = CIBlockSection::GetList(array(), $arFilter);
-    $isSection = $rsSections->Fetch() !== false;
+if(CModule::IncludeModule("iblock")) {
+	global $APPLICATION;
+	$url = $APPLICATION->GetCurPage(false);
+	$iblockID = 3;
+		 
+	function isSectionInCatalog($iblockID, $url) {
+		$breads = explode('/', $url);
+		$code = array_pop(array_filter($breads));
+		$arFilter = array("IBLOCK_ID" => $iblockID, "CODE" => $code);
+		$rsSections = CIBlockSection::GetList(array(), $arFilter);
+		$isSection = $rsSections->Fetch() !== false;
+		return $isSection;
+	}
 }
